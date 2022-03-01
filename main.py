af4856bcc10a24d35dc37b95bd95921bdcf94b11
Rusl4n4ik <russharipov5328059@gmail.com>
2022-03-01 16:16:47 +0500

    init

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-value = {'1': {'Sum':10865, 'Euro': 0.89, 'Rub': 83.82},
-         '2': {'Dollar': 0.012, 'Euro': 0.011, 'Sum': 129.62},
-         '3': {'Dollar': 1.13, 'Rub': 94.50, 'Sum': 12249.20},
-         '4': {'Dollar': 0.000092, 'Euro': 0.000082, 'Rub': 0.0077}
+value = {'1': {'1':10865, '2': 0.89, '3': 83.82},
+         '2': {'1': 0.012, '2': 0.011, '3': 129.62},
+         '3': {'1': 1.13, '2': 94.50, '3': 12249.20},
+         '4': {'1': 0.000092, '2': 0.000082, '3': 0.0077}
          }
 
-#1-Dollar, 2-Rub, 3-Euro, 4-Sum
+# 1-Dollar, 2-Rub, 3-Euro, 4-Sum
+# 1: 1-Sum, 2-Euro, 3-Rub
+# 2: 1- Dollar, 2-Euro, 3-Sum
+# 3: 1-Dollar, 2-Rub, 3-Sum
+# 4: 1-Dollar, 2-Euro, 3-Rub
 
 
 def pidr(value1, value1_c, value2):
@@ -14,15 +18,11 @@ def pidr(value1, value1_c, value2):
 class Sum:
 
     def vl(self):
-        __index = 1
         try:
             value1 = input(" 1.Доллар\n 2.Евро\n 3.Рубль\n 4.Сум\n Выберите валюту: ")
             value1_c = int(input("Введите значение: "))
         except KeyError:
             print("Введите верный ключ")
-        for Value in value[value1]:
-            print(str(__index) + '.' + Value)
-            __index += 1
         value2 = input("Выберите валюту: ")
         value[value1][value2]
         print(pidr(value1,value1_c,value2))
